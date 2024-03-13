506a62ad6865abf9167dac6575369ddb35e93aff
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-03-13 13:56:15 +0100

    templating SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,17 @@
-import "./SearchBar.css"
+import "./SearchBar.css";
 
 export default function SearchBar() {
-    return (
-        <div className="searchBar">
-
-        </div>
-    )
+  return (
+    <div className="searchBar">
+      <section className="searchVariants">
+        <div className="searchPet"></div>
+        <div className="searchBreed"></div>
+      </section>
+      <section className="searchButton">
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <p>Find now</p>
+        </a>
+      </section>
+    </div>
+  );
 }
