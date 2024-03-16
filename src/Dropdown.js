868113ea014aec33daeefe4dff875737ec0bf559
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-03-16 16:23:41 +0100

    search input field

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -4,7 +4,17 @@ export default function Dropdown() {
   return (
     <div className="dropdown">
       <section className="dropdownArea">
-        <div className="petDropdown"></div>
+        <div className="petDropdown">
+          <div className="petInnerContainer">
+            <input
+              className="searchInput"
+              type="search"
+              placeholder="Search..."
+            />
+            <div className="searchableList"></div>
+          </div>
+        </div>
+        <div className="breedDropdown"></div>
       </section>
     </div>
   );
